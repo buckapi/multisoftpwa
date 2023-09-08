71f53209d8db2686689ea5eb5ca9a2ffe5f82956
buckapi <buckapis@gmail.com>
2023-09-08 09:32:02 -0500

    inicio

---
+++ src/app/services/script.store.ts
@@ -0,0 +1,31 @@
+
+interface Scripts {
+    name: string;
+    src: string;
+}  
+export const ScriptStore: Scripts[] = [
+
+
+        { name: 'modernizr', src: 'assets/multisoft/js/vendor/modernizr-3.5.0.min.js' },
+        { name: 'jquery', src: 'assets/multisoft/js/vendor/jquery-1.12.4.min.js' },
+        { name: 'bootstrap', src: 'assets/multisoft/js/bootstrap.bundle.min.js' },
+        { name: 'owlCarousel', src: 'assets/multisoft/js/owl.carousel.min.js' },
+        { name: 'isotope', src: 'assets/multisoft/js/isotope.pkgd.min.js' },
+        { name: 'slick', src: 'assets/multisoft/js/slick.min.js' },
+        { name: 'magnificPopup', src: 'assets/multisoft/js/magnific-popup.js' },
+        { name: 'meanmenu', src: 'assets/multisoft/js/jquery.meanmenu.min.js' },
+        { name: 'metisMenu', src: 'assets/multisoft/js/metisMenu.min.js' },
+        { name: 'niceSelect', src: 'assets/multisoft/js/jquery.nice-select.js' },
+        { name: 'ajaxForm', src: 'assets/multisoft/js/ajax-form.js' },
+        { name: 'wow', src: 'assets/multisoft/js/wow.min.js' },
+        { name: 'counterup', src: 'assets/multisoft/js/jquery.counterup.min.js' },
+        { name: 'waypoints', src: 'assets/multisoft/js/waypoints.min.js' },
+        { name: 'scrollUp', src: 'assets/multisoft/js/jquery.scrollUp.min.js' },
+        { name: 'imagesLoaded', src: 'assets/multisoft/js/imagesloaded.pkgd.min.js' },
+        { name: 'magnificPopup2', src: 'assets/multisoft/js/jquery.magnific-popup.min.js' },
+        { name: 'easypiechart', src: 'assets/multisoft/js/jquery.easypiechart.js' },
+        { name: 'tilt', src: 'assets/multisoft/js/tilt.jquery.js' },
+        { name: 'plugins', src: 'assets/multisoft/js/plugins.js' },
+        { name: 'main', src: 'assets/multisoft/js/main.js' }
+      ];
+      
