523f811e4792879e7ca70bfffe8461d0ae44b0ac
buckapi <buckapis@gmail.com>
2023-09-13 18:03:12 -0500

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ const routes: Routes = [
   { path:'', redirectTo: '/home', pathMatch:'full'},
   { path: 'about', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) }, 
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) }, 
-  { path: 'capacitaciones', loadChildren: () => import('./components/capacitaciones/capacitaciones.module').then(m => m.HomeModule) }, 
-  { path: 'blog', loadChildren: () => import('./components/blog/blog.module').then(m => m.HomeModule) }, 
-  { path: 'clientes', loadChildren: () => import('./components/clientes/clientes.module').then(m => m.HomeModule) }, 
-  { path: 'contact', loadChildren: () => import('./components/contact/contact.module').then(m => m.HomeModule) }, 
+  { path: 'capacitaciones', loadChildren: () => import('./components/capacitaciones/capacitaciones.module').then(m => m.CapacitacionesModule) }, 
+  { path: 'blog', loadChildren: () => import('./components/blog/blog.module').then(m => m.BlogModule) }, 
+  { path: 'clientes', loadChildren: () => import('./components/clientes/clientes.module').then(m => m.ClientesModule) }, 
+  { path: 'contact', loadChildren: () => import('./components/contact/contact.module').then(m => m.ContactModule) }, 
   ];
 
 @NgModule({
