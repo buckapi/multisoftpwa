0884891ed35d66f3aab0d852aebd1abe8848648f
buckapi <buckapis@gmail.com>
2023-09-13 14:46:52 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [ 
+  { path:'', redirectTo: '/home', pathMatch:'full'},
   { path: 'about', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) }, 
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) }, 
   { path: 'capacitaciones', loadChildren: () => import('./components/capacitaciones/capacitaciones.module').then(m => m.HomeModule) }, 
